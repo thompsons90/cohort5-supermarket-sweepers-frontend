b42fd1af3521039431d5c47c4b0355221b8ad79d
Aaron Rodi <metalhead5209@gmail.com>
2022-07-16 13:48:20 -0500

    added router and route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello World</h1>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path='/' />
+        <Route path='/about' />
+        <Route path='/search' />
+      </Routes>
+    </Router>
   );
 }
 
