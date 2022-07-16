19c8b5e2b79a13f3ede40234a0c84c2fe79a5570
Aaron Rodi <metalhead5209@gmail.com>
2022-07-16 14:49:26 -0500

    added and styled links

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
-import {NavContainer} from "../../styles/Nav.style";
+import {
+  NavContainer,
+  ContainerLeft,
+  ContainerRight,
+  ContainerExtended,
+  ContainerInner,
+  NavLinkContainer,
+  NavLink
+} from "../../styles/Nav.style";
+import { Link } from 'react-router-dom';
+
 
 const Nav = () => {
-  return <NavContainer></NavContainer>
+  return(
+    <NavContainer>
+      <ContainerInner>
+        <ContainerLeft>
+        </ContainerLeft>
+        <ContainerRight>
+        <NavLinkContainer>
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/about">About</NavLink>
+              <NavLink to="/search">Search</NavLink>
+          </NavLinkContainer>
+        </ContainerRight>
+      </ContainerInner>
+      <ContainerExtended></ContainerExtended>
+    </NavContainer>
+  )
+   
 }
 
 export default Nav;
--- src/styles/Nav.style.js
+++ src/styles/Nav.style.js
@@ -1,10 +1,61 @@
 import styled from 'styled-components';  
+import { Link } from 'react-router-dom';
 
 export const NavContainer = styled.nav`
     width: 100%;
-    height: 80px;
+    height: 70px;
     background-color: #5FDD97;
     display: flex;
     flex-direction: column;
 
 `;
+
+export const ContainerLeft = styled.div`
+    flex: 70%;
+    display: flex;
+    align-items: center;
+    padding-left: 5%;
+    border-bottom: 3px solid black;
+`
+export const ContainerRight = styled.div`
+    flex: 30%;
+    display: flex;
+    justify-content: flex-end;
+    padding-right: 50px;
+    border-bottom: 3px solid black;
+`
+
+export const ContainerInner = styled.div`
+    width: 100%;
+    height: 80px;
+    display: flex;
+`
+
+export const NavLinkContainer = styled.div`
+    display: flex;
+`;
+
+export const NavLink = styled(Link)`
+    color: black;
+    font-size: large;
+    font-weight: 700;
+    font-family: Arial, Helvetica, sans-serif;
+    text-decoration: none;
+    margin: 10px;
+    padding: .5em;
+    background-color: #E8FFB7;
+    border-radius: 35%;
+    border: 4px solid black;
+    :hover {
+        transform: translateY(-1px);
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+
+`
+
+
+
+
+export const ContainerExtended = styled.div`
+
+`
