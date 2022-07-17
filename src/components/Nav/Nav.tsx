acc7b5ae65edf7c493dc19078f0e129294a61f80
Aaron Rodi <metalhead5209@gmail.com>
2022-07-17 11:22:47 -0500

    added nav styling

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React from "react";
+import { FaBars } from 'react-icons/fa';
 import LogoImg from "../../assets/logo.png";
-import './logo.css';
-import {
-    Nav,
-    NavContainer,
-    NavLogo
-} from './NavElements'
+import "./logo.css";
+import { Nav, NavContainer, NavLogo, Hamburger, NavMenu, NavItem, Links } from "./NavElements";
 
 const NavBar = () => {
   return (
     <div>
-        <Nav>
-            <NavContainer>
-                <NavLogo to="/">
-                  <img className="logo" src={LogoImg}></img>
-                </NavLogo>
-            </NavContainer>
-        </Nav>
+      <Nav>
+        <NavContainer>
+          <NavLogo to="/">
+            <img className="logo" src={LogoImg}></img>
+          </NavLogo>
+          <Hamburger>
+            <FaBars />
+          </Hamburger>
+          <NavMenu>
+            <NavItem>
+              <Links to="/">Home</Links>
+              <Links to="/about">About</Links>
+              <Links to="/search">Search</Links>
+            </NavItem>
+          </NavMenu>
+        </NavContainer>
+      </Nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/Nav/NavElements.js
+++ src/components/Nav/NavElements.js
@@ -1,5 +1,5 @@
 import { Link as LinkR } from 'react-router-dom';
-// import { Link as LinkS } from 'react-scroll';
+import { Link as LinkS } from 'react-scroll';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
@@ -32,4 +32,70 @@ export const NavContainer = styled.div`
 
 export const NavLogo = styled(LinkR)`
     height: 5em;
+`;
+
+export const Hamburger = styled.div`
+    display: none;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-100%, 60%);
+        font-size: 1.5rem;
+        cursor: pointer;
+    }
+`;
+
+export const NavMenu = styled.ul`
+      display: flex;
+      align-items: center;
+      list-style: none;
+      text-align: center;
+      margin-right: -25px;
+
+      @media screen and (max-width: 768px) {
+        display: none;
+      }
+`;
+
+export const NavItem = styled.li`
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+`;
+
+export const Links = styled(LinkS)`
+    /* color: black;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%; 
+    cursor: pointer;
+    
+    :hover {
+         background-color: rgba(0,0,0,0.2)
+    } */
+
+    color: black;
+    font-size: small;
+    font-weight: 700;
+    font-family: Arial, Helvetica, sans-serif;
+    text-decoration: none;
+    margin: 10px;
+    padding: .3em;
+    align-self: center;
+    background-color: #E8FFB7;
+    border-radius: 35%;
+    border: 2.5px solid black;
+    :hover {
+        transform: translateY(-1px);
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+
+    &.acticve{
+        border-bottom: 3px solid black;
+    }
 `
