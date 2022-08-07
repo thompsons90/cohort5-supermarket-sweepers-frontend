6a82985787aa2a55e4721ad668b6d39ae5bbe9a4
vastagon <jacob.yancey29@yahoo.com>
2022-08-07 07:07:46 +0000

    started search cards. All cards visible with info displayed

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import axios from "axios"
+import SearchCard from "../components/SearchCard"
+import "../components/search.css"
+import { useEffect, useState } from "react"
 
 const search = () => {
+  const [meatData, setMeatData] = useState<any[]>()
+  const [allMeatCards, setAllMeatCards] = useState<any[]>()
+
+  useEffect(() =>{
+    axios.get("https://supermarket-sweepers-backend.herokuapp.com/api/groceryitems/meat")
+    .then(response => {
+      setMeatData(response.data.results)
+    })
+  }, [])
+
+  useEffect(() =>{
+    setAllMeatCards(meatData?.map(data =>{
+      return(<SearchCard name={data.name} type={data.type} store={data.store} pricePerLb={data.pricePerLb} option={data.option}/>)
+    }))
+  }, [meatData])
+
+
+
+
+  if (!meatData) return null
+
   return (
-    <div>search</div>
+    <div className='search-container'>
+      {allMeatCards}
+
+    </div>
   )
 }
 
