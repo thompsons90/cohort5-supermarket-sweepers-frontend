f6ca0f72967b653edc5e87a8d3502fdacc2aa08c
vastagon <jacob.yancey29@yahoo.com>
2022-08-18 04:40:12 +0000

    changed form styling using bootstrap. Initial heartbeat call to quickly boot up heroku. Changed some functions around to accomodate bootstrap

--- src/components/SearchCard.tsx
+++ src/components/SearchCard.tsx
@@ -1,3 +1,8 @@
+import { gracefulify } from 'graceful-fs';
+import {Card, CardBody, CardTitle, ListGroup, ListGroupItem} from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 interface MeatData {
     name: string;
     store: string;
@@ -5,48 +10,22 @@ interface MeatData {
     option: string;
     pricePerLb: number;
 }
-import { gracefulify } from 'graceful-fs';
-import {Card, CardBody, CardTitle, CardText, ListGroup, ListGroupItem} from 'reactstrap';
 
 
 export default function SearchCard(props: MeatData){
 
-    
-
-
-
     return(
-        <Card
-        style={{
-            backgroundColor: "#FEFFFF",
-            listStyleType: "none",
-            padding: "2rem",
-            borderRadius: "20px",
-            borderWidth: "2px",
-            borderStyle: "solid"
-        }}
-        >
-
+        <Card>
         <CardBody>
             <CardTitle tag="h5">{props.name}</CardTitle>
         </CardBody>
 
-        <ListGroup style={{ listStyleType: "none", textTransform: "capitalize", marginTop: "1rem" }} flush>
-            <ListGroupItem>Price: ${props.pricePerLb}</ListGroupItem>
-            <ListGroupItem>Store: {props.store}</ListGroupItem>
-            <ListGroupItem>Type: {props.type}</ListGroupItem>
-            <ListGroupItem>Option: {props.option}</ListGroupItem>
+        <ListGroup flush>
+            <ListGroupItem style={{textTransform: "capitalize"}}>Price: ${props.pricePerLb}</ListGroupItem>
+            <ListGroupItem style={{textTransform: "capitalize"}}>Store: {props.store}</ListGroupItem>
+            <ListGroupItem style={{textTransform: "capitalize"}}>Type: {props.type}</ListGroupItem>
+            <ListGroupItem style={{textTransform: "capitalize"}}>Option: {props.option}</ListGroupItem>
         </ListGroup>
         </Card>
     )
 }
-
-
-
-{/* <div className="search-card">
-<div className="search-card-element">Name: {props.name}</div>
-<div className="search-card-element">Store: {props.store}</div>
-<div className="search-card-element">Type: {props.type}</div>
-<div className="search-card-element">Price: ${props.pricePerLb}</div>
-<div className="search-card-element">Option: {props.option}</div>
-</div> */}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from '../components/Hero/Hero';
+import axios from 'axios';
 
 const home = () => {
+
+  useEffect(() =>{
+    axios.get("https://supermarket-sweepers-backend.herokuapp.com/api/heartbeat")
+  }, [])
+
   return (
     <>
       <Hero />
